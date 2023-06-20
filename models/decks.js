9d1a64ae69d1c28b2bb4df7baac0f784ee84ce4f
sam98963 <szwilliamson01@gmail.com>
2023-06-20 21:29:14 +0100

    decks by user added

---
+++ models/decks.js
@@ -0,0 +1,59 @@
+
+import * as userModel from "../models/users";
+
+export async function searchBooksByTitle(req, res, next) {
+  if (req.query.title !== undefined) {
+    const books = await bookModel.searchBooksByTitle(req.query.title);
+    return res.json({ success: true, payload: books });
+  }
+  next();
+}
+
+export async function getUsernameFromUser(req, res, next) {
+  if (req.query.username !== undefined) {
+    const username = await userModel.getUsernameFromUser(req.query.username);
+    return res.json({ success: true, payload: username });
+  }
+}
+
+
+export async function getPasswordFromUser(req, res, next) {
+  if (req.query.password !== undefined) {
+    const password = await userModel.getPasswordFromUser(req.query.password);
+    return res.json({ success: true, payload: password });
+  }
+}
+
+
+
+
+
+
+export async function getUsers(req, res) {
+  const users = await userModel.getUsers();
+  res.json({ success: true, payload: users });
+}
+
+export async function getUserById(req, res) {
+  const user = await userModel.getUserById(req.params.id);
+  res.json({ success: true, payload: user });
+  next();
+}
+
+export async function createUser(req, res) {
+  const data = req.body;
+  const user = await userModel.createUser(data);
+  res.json({ success: true, payload: user });
+
+}
+
+export async function updateUserById(req, res) {
+  const data = req.body;
+  const book = await userModel.updateUserById(req.params.id, data);
+  res.json({ success: true, payload: book });
+}
+
+export async function deleteUserById(req, res) {
+  const user = await userModel.deleteUserById(req.params.id);
+  res.json({ success: true, payload: user });
+}
